8d0d8249ecab84e086b916ca55e931f4ab7c4b6c
Sambo68 <iamireh@aol.com>
2017-09-09 14:53:41 -0400

    updated home.html & survey.html code

---
+++ app/data/friends.js
@@ -0,0 +1,7 @@
+var friends = [ {
+    "name" : "Josh",
+    "photo" : "http://www.mens-hairstylists.com/wp-content/uploads/2015/10/stylish-haircuts-for-men.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}];
+
+module.exports = friends;
